d07821898b26ffa11337a9dd0d281e5740d1ffb8
kasimir <blaser_k@gmx.ch>
2016-02-01 20:59:56 +0100

    make it smaller

--- src/test/java/com/box3000/execute_around_pattern/ThreadSafeUtilTest.java
+++ src/test/java/com/box3000/execute_around_pattern/ThreadSafeUtilTest.java
@@ -8,7 +8,6 @@ import static org.junit.Assert.assertEquals;
 
 public class ThreadSafeUtilTest {
 
-
     @Test
     public void simpleCase() {
         StringBuilder sb = new StringBuilder();
@@ -18,21 +17,20 @@ public class ThreadSafeUtilTest {
 
     @Test
     public void parallelCase() {
-
-        final StringBuilder actual = new StringBuilder();
         final StringBuffer expected = new StringBuffer();
+        final StringBuilder actual = new StringBuilder();
 
-        IntStream.range(0, 100).parallel().forEach(i -> ThreadSafeUtil.withLock(() -> {
-            expected.append("<").append(i).append(">\n");
-            
-            actual.append("<");
-            Thread.sleep(50);
-            actual.append(i);
-            Thread.sleep(50);
-            actual.append(">\n");
-        }));
-        System.out.println(actual);
+        IntStream.range(0, 100).parallel().forEach(i
+                -> ThreadSafeUtil.withLock(() -> {
+                    expected.append("<").append(i).append(">\n");
 
-        assertEquals("Both Strings should be same", expected.toString(), actual.toString());
+                    actual.append("<");
+                    Thread.sleep(50);
+                    actual.append(i);
+                    Thread.sleep(50);
+                    actual.append(">\n");
+                }));
+
+        assertEquals(expected.toString(), actual.toString());
     }
 }
